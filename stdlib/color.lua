1d431e155dec6e4cfa28fa229519102cb8db3daa
R. Richard <ni-richard@protonmail.ch>
2019-07-09 19:16:19 +0200

    Add ASCII color constants

---
+++ stdlib/color.lua
@@ -0,0 +1,26 @@
+--[[
+  **************************
+  * ascii color codes      *
+  * written by: ni-richard *
+  **************************
+]]
+
+color = {
+  black       = "\x1b[30m",
+  grey        = "\x1b[30;1m",
+  red         = "\x1b[31m",
+  boldred     = "\x1b[31;1m",
+  green       = "\x1b[32m",
+  boldgreen   = "\x1b[32;1m",
+  orange      = "\x1b[33m",
+  yellow      = "\x1b[33;1m",
+  blue        = "\x1b[34m",
+  boldblue    = "\x1b[34;1m",
+  magenta     = "\x1b[35m",
+  boldmagenta = "\x1b[35;1m",
+  cyan        = "\x1b[36m",
+  boldcyan    = "\x1b[36;1m",
+  white       = "\x1b[37m",
+  boldwhite   = "\x1b[37;1m",
+  reset       = "\x1b[0m"
+}
